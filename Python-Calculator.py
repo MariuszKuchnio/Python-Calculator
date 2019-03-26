ed3b7989eaaae2441dbdbb22ab9f2a4d07693fdc
Mariusz <kuchnio.mariusz@gmail.com>
2019-03-26 14:41:04 +0100

    Next file xml
    Next file xml

---
+++ Python-Calculator.py
@@ -0,0 +1,13 @@
+print("..:::PYTHON-CALCULATOR:::..")
+print("First number:")
+x=int(input())
+print("Second number:")
+y=int(input())
+print("Good Job! Your numbers: " + str(x) + " and " + str(y))
+print("Now tell me what can i do for you?")
+print(" (+) - Dodawanie")
+print(" (-) - Odejmowanie")
+print(" (*) - Mnożenie")
+print(" (/) - Dzielenie")
+
+
